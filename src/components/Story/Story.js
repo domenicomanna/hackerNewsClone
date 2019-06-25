fcc015ab00a076a764f5e5dc74aa56336fdbc1d3
Domenico Manna <mannadomenico2849@gmail.com>
2019-06-25 15:23:25 -0400

    Display top stories from hackernews

--- src/components/Story/Story.js
+++ src/components/Story/Story.js
@@ -5,7 +5,7 @@ const Story = (props) => {
     return (
         <li className = {styles.story}>
             <h2 className = {styles.storyTitle}>
-                <a href="#" className={styles.storyLink}> {props.title} </a>
+                <a href= {props.url} target = "_blank" className={styles.storyLink}> {props.title} </a>
             </h2>
             <p className = {styles.storyDescription}>{props.score} points by {props.author} </p>
         </li>
--- src/components/StoryList/StoryList.js
+++ src/components/StoryList/StoryList.js
@@ -2,14 +2,25 @@ import React from 'react';
 import styles from './StoryList.module.css';
 import Story from '../Story/Story';
 
-const StoryList = () => {
+const StoryList = (props) => {
     return ( 
         <ol className = {styles.stories}>
-            <Story title = 'Story title' score = '12' author = 'An author' />
-            <Story title = 'Story title' score = '12' author = 'An author' />
-            <Story title = 'Story title' score = '12' author = 'An author' />
+            {transformStories(props.stories)}
         </ol>
     );
+}
+
+function transformStories(stories){
+    return stories.map( (story) => {
+        return (
+            <Story key = {story.id}
+            title = {story.title}
+            url = {story.url}
+            author = {story.by}
+            score = {story.score}/>
+        )
+    })
+    
 }
  
 export default StoryList;
