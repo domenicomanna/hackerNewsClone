88271e363da15129e33e28e6c0ba280fc0d89383
Domenico Manna <mannadomenico2849@gmail.com>
2019-06-25 15:38:27 -0400

    Updates

---
+++ precache-manifest.098477810bf882c974dad8446b25fa03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37509881e2f1380d0ef228ceaf0e090",
+    "url": "/hackerNewsClone/index.html"
+  },
+  {
+    "revision": "b69cac60cd3aca26228c",
+    "url": "/hackerNewsClone/static/css/main.561ca1da.chunk.css"
+  },
+  {
+    "revision": "386ecd2d6563fe5f818b",
+    "url": "/hackerNewsClone/static/js/2.e98b97ad.chunk.js"
+  },
+  {
+    "revision": "b69cac60cd3aca26228c",
+    "url": "/hackerNewsClone/static/js/main.bddcc901.chunk.js"
+  },
+  {
+    "revision": "3f644a8f9330c3e88b3d",
+    "url": "/hackerNewsClone/static/js/runtime~main.9968f949.js"
+  }
+]);
